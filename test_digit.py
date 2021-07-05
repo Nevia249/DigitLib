e60a3756d135f92fbfec8e937004f87c01fbf054
Nevia249 <nevia24@gmail.com>
2021-07-05 13:00:03 +0200

    dodaj untit test

--- test_digit.py
+++ test_digit.py
@@ -1,9 +1,14 @@
 import digit
+#import pyttest
 
-class TestDigi:
+class TestDigit:
+
     def test_add(self):
-        assert 4==digit.dsum(2,2)
-    def test_sub(self):
-        assert 2==digit.dsub(4,2)
+        assert 4 == digit.add(2,2)
 
-        
+    def test_sub(self):
+        assert 2 == digit.sub(4,2)
+##
+##if __name__ == "__main__":
+##    pytest.main()
+    
